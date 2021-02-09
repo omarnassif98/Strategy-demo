9573c6e31ae71ecd3c88e8e86fd6c9bfd9440fce
omarnassif98 <omar.nassif98@gmail.com>
2021-02-08 21:48:17 -0500

    Added province info
    - web server now serves up province info
    - provinces now display their neighbors when clicked

--- flask/server.py
+++ flask/server.py
@@ -10,5 +10,11 @@ def SendEuropeSVG():
     print('SENDING THE SVG... HERE\'S HOPING!')
     return send_from_directory('static', 'europe stable.svg')
 
+
+@app.route('/provinceData')
+def SendProvinceData():
+    print('SENDING JSON... HERE GOES')
+    return send_from_directory('static', 'provinceInfo.json')
+
 if __name__ == '__main__':  # pragma: no cover
     app.run(port=8080, debug=True)
--- flask/static/SVGLoader.js
+++ flask/static/SVGLoader.js
@@ -1,20 +1,45 @@
 console.log('Starting');
-const resourceRequest = new XMLHttpRequest();
-resourceRequest.open('GET', window.origin + '/europe');
-resourceRequest.send();
-resourceRequest.onreadystatechange = function() {
-    if(resourceRequest.readyState === XMLHttpRequest.DONE){
-        const resXML = new DOMParser().parseFromString(resourceRequest.response, 'image/svg+xml');
-        const svgObj = resXML.getElementsByTagName('svg')[0];
-        svgObj.querySelectorAll('path').forEach(element => {
-            console.log(element);
-            const provID = element.getAttribute('id');
-            element.removeAttribute('style');
-        });
-        const map = document.getElementById('mapdiv');
-        map.appendChild(svgObj);
-        
-    }else{
-        console.log(resourceRequest.statusText);
+var provinceInfo = {}
+LoadSVG();
+console.log('loaded svg');
+LoadProvinceMetadata();
+console.log('loaded metadata');
+function LoadSVG() {
+        const resourceRequest = new XMLHttpRequest();
+        resourceRequest.open('GET', window.origin + '/europe');
+        resourceRequest.send();
+        resourceRequest.onreadystatechange = function() {
+            if(resourceRequest.readyState === XMLHttpRequest.DONE){
+                const resXML = new DOMParser().parseFromString(resourceRequest.response, 'image/svg+xml');
+                const svgObj = resXML.getElementsByTagName('svg')[0];
+                svgObj.querySelectorAll('path').forEach(element => {
+                    const provID = element.getAttribute('id');
+                    element.removeAttribute('style');
+                    element.addEventListener('click',function(){
+                        ProvinceSelect(provID)
+                    });
+                });
+                const map = document.getElementById('mapdiv');
+                map.appendChild(svgObj);
+            }
+        }
     }
+
+function LoadProvinceMetadata() {
+        const resourceRequest = new XMLHttpRequest();
+        resourceRequest.open('GET', window.origin + '/provinceData');
+        resourceRequest.send();
+        resourceRequest.onreadystatechange = function() {
+            if(resourceRequest.readyState === XMLHttpRequest.DONE){
+                const resJSON = JSON.parse(resourceRequest.response);
+                for (let key in resJSON){
+                    console.log([key, resJSON[key]]);
+                    provinceInfo[key] = resJSON[key]
+                }
+            }
+        }
+    }
+
+function ProvinceSelect(provID){
+    console.log([provID, provinceInfo[provID]]);
 }
