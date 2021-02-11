cbdd1f0dbc47d21ca963e15c629fdde7fc9ad082
omarnassif98 <omar.nassif98@gmail.com>
2021-02-11 11:24:33 -0500

    Delete GameLogic.js

--- GameLogic.js
+++
@@ -1,4 +0,0 @@
-function UpdateLastClicked(obj){
-    const territory = obj.childNodes[1];
-    document.getElementById("LastClicked").innerHTML = "Clicked on a <span style='color:red;'>" + territory.getAttribute("name") + "</span>";
-}
