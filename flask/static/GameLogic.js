0c5d27fab0950d01fc58443929131be8f9c4df9c
omarnassif98 <omar.nassif98@gmail.com>
2021-02-19 01:50:09 -0500

    Added game state and expansion
    Added expansion functionality

--- flask/static/GameLogic.js
+++ flask/static/GameLogic.js
@@ -3,22 +3,59 @@ var gameInfo = {
     "nationInfo":{},
     "turnNumb":-1,
     "deadline": "01 September 1939 12:00 GMT",
-    "gameState":0,
-    "playingAs":""}
+    "focused":false,
+    "lastFocused":"",
+    "turnComplete":false,
+    "playingAs":"ITA"
+}
 const baseURL = window.origin;
 SetupGame();
+
 async function SetupGame(){
     await LoadGameConfiguration();
     await LoadSVG();
-}
-
-for (countryTag in gameInfo.nationInfo){
-    for(prov in gameInfo.nationInfo[countryTag].provinces){
-        print(countryTag + ' - ' + prov);
+    
+    for(nationID in gameInfo.nationInfo){
+        gameInfo.nationInfo[nationID].provinces.forEach(provID => {
+            const element = document.getElementById(provID);
+            element.style.fill = gameInfo.nationInfo[nationID].color;
+        });
     }
+    
+    EnableProvinces(gameInfo.nationInfo[gameInfo.playingAs].provinces);
 }
 
+function EnableProvinces(provIDs){
+    const svgObj = document.getElementById('svg51');
+    provIDs.forEach(provID => {
+        const pathReference = document.getElementById(provID);
+        pathReference.classList.add('enabledProvince');
+        svgObj.removeChild(pathReference);
+        svgObj.appendChild(pathReference)
+    });
+}
+function DisableProvinces(provIDs){
+    const svgObj = document.getElementById('svg51');
+    provIDs.forEach(provID => {
+        if(!gameInfo.nationInfo[gameInfo.playingAs].provinces.includes(provID)){
+            const pathReference = document.getElementById(provID);
+            pathReference.classList.remove('enabledProvince');
+            svgObj.removeChild(pathReference);
+            svgObj.insertBefore(pathReference, svgObj.firstChild)
+    }
+    });
+}
 
+function AddTerritoryToNation(nationID, provIDs){
+    console.log(`Actually adding territory to ${nationID}`);
+    provIDs.forEach(provID => {
+        console.log(`Adding ${provID} to ${nationID}`);
+        const element = document.getElementById(provID);
+        element.style.fill = gameInfo.nationInfo[nationID].color;
+        gameInfo.nationInfo[nationID].provinces.push(provID);
+        //gameInfo.provinceInfo[provID].owner = nationID;
+    });
+}
 async function LoadGameConfiguration(){
     const resJSON = JSON.parse(await ResourceRequest(baseURL +  '/gameState'));
     for(let key in resJSON){
@@ -31,12 +68,9 @@ async function LoadSVG() {
         const svgObj = resXML.getElementsByTagName('svg')[0];
         svgObj.querySelectorAll('path').forEach(element => {
             const provID = element.getAttribute('id');
-            console.log(`${provID} is a province`);
+            
             element.removeAttribute('style');
             element.classList.add('province');
-            if(gameInfo.provinceInfo[provID]){
-                element.style.fill = gameInfo.nationInfo[gameInfo.provinceInfo[provID].owner].color;
-            }
             element.addEventListener('click',function(){
                 ProvinceSelect(provID);
             });
@@ -47,12 +81,38 @@ async function LoadSVG() {
 
 
 function ProvinceSelect(provID){
-    console.log(gameInfo.provinceInfo[provID]);
+    console.log(provID);
+    if(gameInfo.nationInfo[gameInfo.playingAs].provinces.includes(provID) && !gameInfo.focused){
+        FocusProvince(provID);
+        gameInfo.focused = true;
+        gameInfo.lastFocused = provID;
+    }else{
+        ResetFocus();
+        if(gameInfo.provinceInfo[gameInfo.lastFocused].neighbors.includes(provID)){
+            
+            const copy = gameInfo.playingAs.valueOf();
+            console.log(`should be adding territory to ${copy}`);
+            AddTerritoryToNation(copy, [provID]);
+        }
+        DisableProvinces(gameInfo.provinceInfo[gameInfo.lastFocused].neighbors);
+        gameInfo.focused = false;
+    }
 }
 
-function FocusProvince(){
-    const allProvs = document.getElementsByClassName('mapDiv');
-    for (e in allProvs){
-        e.removeAttribute()
-    }
+function FocusProvince(provID){
+    const allProvs = [...document.getElementsByTagName('path')];
+    const whiteList = gameInfo.provinceInfo[provID].neighbors;
+    allProvs.forEach(e => {
+        if(!(whiteList.includes(e.getAttribute('id')) ||  e.getAttribute('id') === provID)){
+            e.classList.add("disabledProvince");
+        }
+    });
+    EnableProvinces(whiteList);
+}
+
+function ResetFocus(){
+    const allProvs = [...document.getElementsByTagName('path')];
+    allProvs.forEach(e => {
+            e.classList.remove("disabledProvince");
+    });
 }
