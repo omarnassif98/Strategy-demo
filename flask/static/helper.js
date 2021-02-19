4e4d00f5c8e86736487f99dbe0a9d578ea697743
Abou Nassif Mourad <abounassifmourado@wit.edu>
2021-02-18 19:30:59 -0500

    FIXED EVERYTHING
    Is now asynchronous

---
+++ flask/static/helper.js
@@ -0,0 +1,16 @@
+function ResourceRequest(url){
+    return new Promise ((resolve, reject) => {
+        const req = new XMLHttpRequest();
+        req.open('GET', url);
+        req.send();
+        req.onreadystatechange = function(){
+            if(req.readyState === XMLHttpRequest.DONE){
+                if(req.status == 200){
+                    resolve(req.response);
+                }else{
+                    reject(req.response);
+                }
+            }
+        }
+    });
+}
